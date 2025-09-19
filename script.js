cf231ec66519c6bf29256015a02a2330340f0763
Nevin <n.boyd1@yahoo.com>
2025-09-19 13:08:12 -0700

    Added styling to button, including hover, colors

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
+const resultsDiv = document.getElementById("results");
+
