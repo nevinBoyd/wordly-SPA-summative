5df0fc00e9a40052d57a93e43f2f5bfb64099bec
Nevin <n.boyd1@yahoo.com>
2025-09-19 19:52:36 -0700

    Cleaning up the left overs from restructuing

--- script.js
+++
@@ -1,6 +0,0 @@
-const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-
-const form = document.getElementById("search-form");
-const input = document.getElementById("search-input");
-const resultsDiv = document.getElementById("results");
-
